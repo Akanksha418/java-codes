3da06196230fb30b0c7b8e864696896b52f33f2c
akanksha418 <chouguleakanksha24@gmail.com>
2025-02-25 11:13:28 +0530

    practice

---
+++ expresion_1.java
@@ -0,0 +1,12 @@
+public class expresion_1{
+    public static void main(String[] args){
+        String infix = "2 + 3 * 4";
+        String prefix = "+ 2 * 3 4";
+        String postfix = "2 3 4 * +";
+
+        System.out.println("infix" + infix);
+        System.out.println("prefix" + prefix);
+        System.out.println("postfix" + postfix);
+    } 
+
+}
---
+++ expression.java
@@ -0,0 +1,15 @@
+public class expression {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // Postfix increment
+        System.out.println(number);  
+        System.out.println(number++);
+        System.out.println(number); 
+
+        // Prefix decrement
+        System.out.println(number);  
+        System.out.println(number--); 
+        System.out.println(number);  
+    }
+}
